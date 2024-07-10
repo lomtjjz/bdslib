abfeddfd7be81bb539d9bd644bd6caa3e39af85d
Jan Zachar <lomtjjzAM@proton.me>
2024-07-10 17:25:18 +0200

    First list implementation

--- include/list.h
+++ include/list.h
@@ -1,3 +1,17 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef __LIST_H
 #define __LIST_H
 #include <stdbool.h>
@@ -18,32 +32,35 @@ typedef struct {
 
 
 
-extern list_t list_new();
+extern void list_new(list_t*);
 
 
 extern bool list_empty(list_t);
 extern size_t list_size(list_t);
 
 
-extern int list_insert(list_t*, void*, size_t);
 extern int list_push_front(list_t*, void*);
 extern int list_push_back(list_t*, void*);
+extern int list_insert(list_t*, void*, size_t);
 
 
-extern void *list_erase(list_t*, size_t);
 extern void *list_pop_front(list_t*);
 extern void *list_pop_back(list_t*);
+extern void *list_erase(list_t*, size_t);
 
 
 extern void *list_front(list_t);
 extern void *list_back(list_t);
+extern void *list_at(list_t, size_t);
 
 
-extern list_node list_it_begin(list_t);
-extern list_node list_it_end(list_t);
-extern list_node list_it_prev(list_node);
-extern list_node list_it_next(list_node);
+extern struct list_node *list_sentinel(list_t*);
+extern void *list_node_data(struct list_node*);
+extern struct list_node *list_node_prev(struct list_node*);
+extern struct list_node *list_node_next(struct list_node*);
 
+extern int list_node_insert(struct list_node*, void*);
+extern void *list_node_erase(struct list_node*);
 
 
 #endif
