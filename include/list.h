ceae934fc88ed469363ce9772c2f38a31dd880fc
Jan Zachar <lomtjjzAM@proton.me>
2024-07-10 21:23:56 +0200

    Formatted header

--- include/list.h
+++ include/list.h
@@ -18,7 +18,8 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 #include <stddef.h>
 
 
-
+// List node.
+// You may encounter it if you try iterating the list.
 struct list_node {
         struct list_node *sentinel;
         struct list_node *prev;
@@ -27,43 +28,79 @@ struct list_node {
         void *data;
 };
 
+// The list itself.
+// You must initialise it with `list_new()`.
 typedef struct {
         struct list_node sentinel;
         size_t size;
 } list_t;
 
 
-
-extern void list_new(list_t*);
-
-
-extern bool list_empty(list_t);
-extern size_t list_size(list_t);
+// Initialises the list. 
+// Must be used against newly created variables before performing any
+// operations.
+extern void                             list_new(list_t*);
 
 
-extern int list_push_front(list_t*, void*);
-extern int list_push_back(list_t*, void*);
-extern int list_insert(list_t*, void*, size_t);
+// Returns true, if list is empty.
+extern bool                             list_empty(list_t);
+
+// Returns list size.
+extern size_t                           list_size(list_t);
 
 
-extern void *list_pop_front(list_t*);
-extern void *list_pop_back(list_t*);
-extern void *list_erase(list_t*, size_t);
+// Inserts new element at the beginning.
+// Returns nonzero value on failure.
+extern int                              list_push_front(list_t*, void*);
+
+// Inserts new element at the end.
+// Returns nonzero value on failure.
+extern int                              list_push_back(list_t*, void*);
+
+// Inserts new element at specified index.
+// Returns nonzero value on failure.
+extern int                              list_insert(list_t*, void*, size_t);
+
+// Removes the first element and returns it.
+// If empty, returns NULL.
+extern void*                            list_pop_front(list_t*);
+
+// Removes the last element and returns it.
+// If empty, returns NULL.
+extern void*                            list_pop_back(list_t*);
+
+// Removes the element at specified position and returns it.
+// If list is too small, returns NULL.
+extern void*                            list_erase(list_t*, size_t);
 
 
-extern void *list_front(list_t);
-extern void *list_back(list_t);
-extern void *list_at(list_t, size_t);
+// Returns the element at the specified position.
+// If list is too small, returns NULL.
+extern void*                    list_at(list_t, size_t);
+
+// Returns the first element.
+// If empty, returns NULL.
+extern void*                    list_front(list_t);
+
+// Returns the last element.
+// If empty, returns NULL.
+extern void*                    list_back(list_t);
 
 
+// Returns sentinel node of a list.
+extern struct list_node*        list_sentinel(list_t*);
 
-extern struct list_node *list_sentinel(list_t*);
-extern void *list_node_data(struct list_node*);
-extern struct list_node *list_node_prev(struct list_node*);
-extern struct list_node *list_node_next(struct list_node*);
+// Returns data stored in a node.
+extern void*                    list_node_data(struct list_node*);
 
-extern int list_node_insert(struct list_node*, void*);
-extern void *list_node_erase(struct list_node*);
+// Returns previous node.
+extern struct list_node*        list_node_prev(struct list_node*);
+
+// Returns next node.
+extern struct list_node*        list_node_next(struct list_node*);
+
+extern int                      list_node_insert(struct list_node*, void*);
+extern void*                    list_node_erase(struct list_node*);
 
 
 
